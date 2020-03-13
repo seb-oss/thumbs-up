e99f3df3277a31506eec9ea1f6d42d7eb709514f
Stuart Webster <stuart.webster@seb.se>
2020-03-13 16:17:26 +0100

    add upsert_thumb SQL function

--- docker-entrypoint-initdb.d/create-schema.sql
+++ docker-entrypoint-initdb.d/create-schema.sql
@@ -26,22 +26,47 @@ CREATE VIEW thumbs_up AS
     JOIN github_users g ON g.id = t.user_id
     JOIN page_urls p ON p.id = t.url_id;
 
-CREATE FUNCTION insert_thumb()
+CREATE FUNCTION upsert_thumb()
     RETURNS trigger AS
 $$
-DECLARE url_id INT;
-DECLARE user_id INT;
 BEGIN
-    INSERT INTO page_urls(page_url) SELECT NEW.page_url RETURNING id into url_id;
-    INSERT INTO github_users(github_user) SELECT NEW.github_user RETURNING id into user_id;
-    INSERT INTO thumbs(url_id, user_id, thumb_up) VALUES(url_id, user_id, NEW.thumb_up);
+    WITH inserted_url AS (
+        INSERT INTO page_urls(page_url)
+        SELECT NEW.page_url
+        ON CONFLICT(page_url) DO NOTHING
+        RETURNING id, page_url
+    ), inserted_user AS (
+        INSERT INTO github_users(github_user)
+        SELECT NEW.github_user
+        ON CONFLICT(github_user) DO NOTHING
+        RETURNING id, github_user
+    ), upserted_url AS (
+        SELECT id, page_url
+        FROM page_urls
+        UNION ALL
+        SELECT id, page_url
+        FROM inserted_url
+    ), upserted_user AS (
+        SELECT id, github_user
+        FROM github_users
+        UNION ALL
+        SELECT id, github_user
+        FROM inserted_user
+    )
+    INSERT INTO thumbs(url_id, user_id, thumb_up)
+    SELECT upserted_url.id, upserted_user.id, new_row.thumb_up
+    FROM (SELECT NEW.*) new_row
+    JOIN upserted_url USING (page_url)
+    JOIN upserted_user USING (github_user)
+    ON CONFLICT(url_id, user_id)
+        DO UPDATE SET thumb_up = NEW.thumb_up;
     RETURN NEW;
 END;
 $$ language plpgsql;
 
-CREATE TRIGGER insert_thumb_trigger
-INSTEAD OF INSERT ON thumbs_up
-FOR EACH ROW EXECUTE FUNCTION insert_thumb();
+CREATE TRIGGER upsert_thumb_trigger
+INSTEAD OF INSERT OR UPDATE ON thumbs_up
+FOR EACH ROW EXECUTE FUNCTION upsert_thumb();
 
 INSERT INTO thumbs_up(page_url, github_user, thumb_up)
 VALUES('hej', 1000, true);
