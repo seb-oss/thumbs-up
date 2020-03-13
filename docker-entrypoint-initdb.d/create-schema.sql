ee06a7b6ed6dd632eacbfab41a2fe66a8ccbef70
Stuart Webster <stuart.webster@seb.se>
2020-03-13 11:41:07 +0100

    correct initial database schema

--- docker-entrypoint-initdb.d/create-schema.sql
+++ docker-entrypoint-initdb.d/create-schema.sql
@@ -1,51 +1,47 @@
-CREATE table github_users (
+
+CREATE TABLE page_urls (
     id SERIAL PRIMARY KEY,
-    github_id INT UNIQUE
+    page_url TEXT UNIQUE
 );
 
-CREATE table page_urls (
+CREATE TABLE github_users (
     id SERIAL PRIMARY KEY,
-    url TEXT UNIQUE
+    github_user INT UNIQUE
 );
 
-CREATE table thumbs_up (
+CREATE TABLE thumbs (
     id SERIAL PRIMARY KEY,
     url_id INT REFERENCES page_urls(id),
-    github_user_id INT REFERENCES github_users(id),
-    sentiment BOOLEAN
-    CONSTRAINT UNIQUE(url_id, github_user_id)
+    user_id INT REFERENCES github_users(id),
+    thumb_up BOOLEAN,
+    UNIQUE(url_id, user_id)
 );
 
-
-
-CREATE view thumbs AS
+CREATE VIEW thumbs_up AS
     SELECT
-        u.url,
-        g.github_id,
-        t.sentiment
-    FROM github_users g
-    JOIN thumbs_up t ON g.id = t.github_user_id
-    JOIN page_urls u ON u.id = t.url_id
-
-
-INSERT INTO github_users(github_id) VALUES (1000);
-INSERT INTO page_urls(url) VALUES ('hej');
-INSERT INTO thumbs_up(url_id, github_user_id, sentiment) VALUES(1, 1, true);
+        p.page_url,
+        g.github_user,
+        t.thumb_up
+    FROM thumbs t
+    JOIN github_users g ON g.id = t.user_id
+    JOIN page_urls p ON p.id = t.url_id;
 
 CREATE FUNCTION insert_thumb()
     RETURNS trigger AS
 $$
-DECLARE page_url_id INT;
-DECLARE github_user_id INT;
+DECLARE url_id INT;
+DECLARE user_id INT;
 BEGIN
-    INSERT INTO github_users (github_id) SELECT NEW.github_id RETURNING id into github_user_id;
-    INSERT INTO page_urls(url) SELECT NEW.url RETURNING id into page_url_id;
-    INSERT INTO thumbs_up(sentiment, url_id, github_user_id) VALUES(NEW.sentiment, page_url_id, github_user_id);
+    INSERT INTO page_urls(page_url) SELECT NEW.page_url RETURNING id into url_id;
+    INSERT INTO github_users(github_user) SELECT NEW.github_user RETURNING id into user_id;
+    INSERT INTO thumbs(url_id, user_id, thumb_up) VALUES(url_id, user_id, NEW.thumb_up);
     RETURN NEW;
 END;
 $$ language plpgsql;
 
+CREATE TRIGGER insert_thumb_trigger
+INSTEAD OF INSERT ON thumbs_up
+FOR EACH ROW EXECUTE FUNCTION insert_thumb();
 
-create trigger insert_thumb_trigger
-instead of insert on thumbs
-for each row execute function insert_thumb();
+INSERT INTO thumbs_up(page_url, github_user, thumb_up)
+VALUES('hej', 1000, true);
--- postgresAdapter.js
+++ postgresAdapter.js
@@ -1,14 +1,17 @@
 const { Client } = require("pg");
 const client = new Client(
-  "postgresql://thumbs-up:supersecret@localhost:5432/thumbs-up"
+  "postgresql://thumbs_up:supersecret@localhost:5432/thumbs_up"
 );
 
 client.connect();
 
 module.exports = {
   async get_thumb(githubId, pageUrl) {
-    const { rows } = await client.query("SELECT sentiment FROM thumbs WHERE github_id = $1 AND url = $2", [githubId, pageUrl]);
+    const { rows } = await client.query(
+      "SELECT thumb_up FROM thumbs_up WHERE github_user = $1 AND page_url = $2",
+      [githubId, pageUrl]
+    );
 
-    return rows[0].sentiment;
+    return rows[0].thumb_up;
   }
 };
