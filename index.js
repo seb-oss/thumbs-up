1389924f4bfa9e6cc773937bc15ff974e3fdfbee
Stuart Webster <stuart.webster@seb.se>
2020-03-16 15:29:03 +0100

    implement postgresAdapter functions

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// const pgAdapter = require('./postgresAdapter')
+const pgAdapter = require('./postgresAdapter')
 const app = require("express")();
 const { authorizeUser } = require("./routes");
 const bodyParser = require("body-parser");
 const PORT = 5000;
-
-// pgAdapter.get_thumb("user", "hej1").then(console.log)
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 app.use(bodyParser.json());
@@ -14,22 +12,28 @@ app.get("/", (req, res) => res.send("I'm alive!"));
 app.get("/authorize", authorizeUser);
 
 app.get("/thumbs", (req, res) => {
-  const { page_url } = req.query;
+  const { pageUrl } = req.query;
 
   res.sendStatus(200);
 });
 
-function getThumbsForPage(page_url) {
-  // return users and their sentiment for the specified page
+function getThumbsForPage(pageUrl, userId) {
+  // For the given page, return total thumbs up, total thumbs down and
+  // sentiment for given user (if defined, otherwise null)
+  pgAdapter.get_thumbs(pageUrl, userId);
 }
 
-function addThumbUpForPage(page_url, userId) {
+function addThumbUpForPage(pageUrl, userId) {
   // Add user with sentiment thumbs_up to page and return boolean
-}
-function addThumbDownForPage(page_url, userId) {
-  // Add user with sentiment thumbs_down to page and return boolean
+  pgAdapter.set_thumb(pageUrl, userId, true);
 }
 
-function removeThumbForPage(page_url, userId) {
+function addThumbDownForPage(pageUrl, userId) {
+  // Add user with sentiment thumbs_down to page and return boolean
+  pgAdapter.set_thumb(pageUrl, userId, false);
+}
+
+function removeThumbForPage(pageUrl, userId) {
   // Remove user from page
+  pgAdapter.delete_thumb(pageUrl, userId);
 }
--- postgresAdapter.js
+++ postgresAdapter.js
@@ -2,16 +2,30 @@ const { Client } = require("pg");
 const client = new Client(
   "postgresql://thumbs_up:supersecret@localhost:5432/thumbs_up"
 );
-
 client.connect();
 
 module.exports = {
-  async get_thumb(githubId, pageUrl) {
+  async get_thumbs(pageUrl, userId) {
     const { rows } = await client.query(
-      "SELECT thumb_up FROM thumbs_up WHERE github_user = $1 AND page_url = $2",
-      [githubId, pageUrl]
+      "SELECT thumbs_up, thumbs_down, user_thumb_up FROM total_thumbs($1, $2)",
+      [pageUrl, userId]
     );
+    return rows[0];
+  },
 
-    return rows[0].thumb_up;
+  async set_thumb(pageUrl, userId, thumbUp) {
+    const { rows } = await client.query(
+      "INSERT INTO thumbs_up(page_url, github_user, thumb_up) VALUES($1, $2, $3)",
+      [pageUrl, userId, thumbUp]
+    );
+    return rows[0];
+  },
+
+  async delete_thumb(pageUrl, userId) {
+    const { rows } = await client.query(
+      "DELETE FROM thumbs_up WHERE page_url = $1 AND github_user = $2",
+      [pageUrl, userId]
+    );
+    return rows[0];
   }
 };
