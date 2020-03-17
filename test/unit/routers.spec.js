78690fe6b348f5172d3c49f7513f53498b1c108d
Mikael Gråborg <mikael.graborg@seb.se>
2020-03-17 16:29:22 +0100

    chore: add chai and example tests

--- test/unit/routers.spec.js
+++ test/unit/routers.spec.js
@@ -1,5 +1,14 @@
 const routes = require(process.cwd() + "/lib/routes");
+const { expect } = require("chai");
 
 describe("routes/getThumbs", () => {
-  routes.convertSentimentToThumb("hej");
+  it("returns thumbUp if param is true", () => {
+    expect(routes.convertSentimentToThumb(true)).to.eql("thumbUp");
+  });
+  it("returns thumbDown if param is false", () => {
+    expect(routes.convertSentimentToThumb(false)).to.eql("thumbDown");
+  });
+  it("returns null if param is null", () => {
+    expect(routes.convertSentimentToThumb(null)).to.eql(null);
+  });
 });
