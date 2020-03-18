196ea8e2cc3accd4277f040a2acf4acd9e015519
Mikael Gråborg <mikael.graborg@seb.se>
2020-03-18 17:40:21 +0100

    test: add downThumb usecase

--- test/e2e/index.spec.js
+++ test/e2e/index.spec.js
@@ -3,13 +3,14 @@ const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const app = require("../../lib/index");
 const expect = chai.expect;
+const { token } = require("./settings");
 
 describe("POST /thumbs", () => {
   it("adds a thumbs up to examples page", done => {
     chai
       .request(app)
       .post("/thumbs")
-      .set("Cookie", "token=")
+      .set("Cookie", `token=${token}`)
       .send({
         userThumb: "thumbUp",
         pageUrl: "examples"
@@ -20,6 +21,26 @@ describe("POST /thumbs", () => {
           thumbsUp: 1,
           thumbsDown: 0,
           userThumb: "thumbUp"
+        });
+        done();
+      });
+  });
+
+  it("adds a thumbs down to new_examples page", done => {
+    chai
+      .request(app)
+      .post("/thumbs")
+      .set("Cookie", `token=${token}`)
+      .send({
+        userThumb: "thumbDown",
+        pageUrl: "new_examples"
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.deep.equal({
+          thumbsUp: 0,
+          thumbsDown: 1,
+          userThumb: "thumbDown"
         });
         done();
       });
