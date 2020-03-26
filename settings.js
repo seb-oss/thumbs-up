e39ec37122a9955641aab342ca57e862150fc528
Mikael Gråborg <mikael.graborg@seb.se>
2020-03-26 09:45:07 +0100

    chore: add settings.js with process.envs

---
+++ settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+  githubUrl: process.env.githubUrl,
+  client_id: process.env.client_id,
+  client_secret: process.env.client_secret,
+  state_password: process.env.state_password,
+  dbHost: process.env.db_host,
+  dbPassword: process.env.db_password,
+  dbUser: process.env.db_user,
+  mockGithub: false
+};
