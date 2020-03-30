3d86a7f86cdce2fb5bd6bbab2557cf0efebf1228
Stuart Webster <stuart.webster@seb.se>
2020-03-30 22:22:23 +0200

    chore: reinstate wait_for_postgres entrypoint

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,12 @@ WORKDIR /app
 
 COPY package.json .npmrc* ./
 
-
 COPY lib ./lib
 COPY public ./public
 COPY settings.js ./
 
 RUN npm i
 
+COPY wait_for_postgres.sh /
+ENTRYPOINT [ "/wait_for_postgres.sh" ]
 CMD [ "npm", "start" ]
--- wait_for_postgres.sh
+++ wait_for_postgres.sh
@@ -1,15 +1,11 @@
-#!/bin/bash
+#!/bin/ash
 
 set -e
 
-host="$1"
-shift
-cmd="$@"
-
-until </dev/tcp/$host/5432; do
+until nc -z $db_host 5432; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "Postgres is up - executing command"
-exec $cmd
+exec $@
